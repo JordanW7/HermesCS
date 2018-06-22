7ce91347ba947ca042d21a3b71f900e84d19d473
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-06-22 14:21:24 +1200

    commit box finished

--- src/components/Requests/RequestViewer/RequestCommentList.js
+++ src/components/Requests/RequestViewer/RequestCommentList.js
@@ -1,89 +1,40 @@
-import React, { Component } from "react";
-import { Row, Col, Input, Button } from "antd";
-import apiBackEnd from "../../../api/api";
-const { TextArea } = Input;
+import React from "react";
+import { Row, Col } from "antd";
+import moment from "moment";
 
-class RequestCommentList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      commentBox: ""
-    };
-  }
-  onCommentChange = event => {
-    this.setState({ commentBox: event.target.value });
-    console.log(event.target.value);
-  };
-  onCommentSubmit = async () => {
-    if (!this.state.commentBox) {
-      return;
-    }
-    const { firstname, lastname, account } = this.props.user.user;
-    const response = await apiBackEnd("addrequestcomments", "post", {
-      comment: this.state.commentBox,
-      account: account,
-      user: `${firstname} ${lastname}`
-    });
-    console.log("RESPONSE", response);
-    console.log("SUBMIT");
-  };
-  render() {
-    const { comments } = this.props;
-    return (
-      <div>
-        <Row>
-          <Col span={3} className="requestviewer-comments-label">
-            Time/Date:
-          </Col>
-          <Col span={5} className="requestviewer-comments-label">
-            User:
-          </Col>
-          <Col span={16} className="requestviewer-comments-label">
-            Comments:
-          </Col>
-        </Row>
-        {comments.map((comment, i) => {
-          return (
-            <Row key={i}>
-              <Col
-                span={3}
-                key={`${i}_time`}
-                className="requestviewer-comments-data"
-              >
-                {comment[0]}
-              </Col>
-              <Col
-                span={5}
-                key={`${i}_user`}
-                className="requestviewer-comments-data"
-              >
-                {`${comment[1]} (${comment[2]})`}
-              </Col>
-              <Col
-                span={16}
-                key={`${i}_comment`}
-                className="requestviewer-comments-data"
-              >
-                {comment[3]}
-              </Col>
-            </Row>
-          );
-        })}
-        <br />
-        <Row>
-          <Col span={2} style={{ textAlign: "center" }}>
-            <label>Comments:</label>
-          </Col>
-          <Col span={20}>
-            <TextArea placeholder="" onChange={this.onCommentChange} />
-          </Col>
-          <Col span={2} style={{ textAlign: "center" }}>
-            <Button onClick={this.onCommentSubmit}>Submit</Button>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+const RequestCommentList = props => {
+  const { comments } = props;
+  return (
+    <div>
+      {comments.map((comment, i) => {
+        return (
+          <Row key={i}>
+            <Col
+              span={3}
+              key={`${i}_time`}
+              className="requestviewer-comments-data"
+            >
+              {moment(comment.created_at).format("DD-MM-YYYY hh:mm")}
+            </Col>
+            <Col
+              span={5}
+              key={`${i}_user`}
+              className="requestviewer-comments-data"
+            >
+              {`${comment.created_by} (${comment.team})`}
+            </Col>
+            <Col
+              span={16}
+              key={`${i}_comment`}
+              className="requestviewer-comments-data"
+            >
+              {comment.comments}
+            </Col>
+          </Row>
+        );
+      })}
+    </div>
+  );
+};
 
 export default RequestCommentList;
--- src/components/Requests/RequestViewer/RequestViewer.js
+++ src/components/Requests/RequestViewer/RequestViewer.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import RequestForm from "../../RequestForm/RequestForm";
 import apiBackEnd from "../../../api/api";
+import { Row, Col, Input, Button } from "antd";
 import RequestCommentList from "./RequestCommentList";
 import "./RequestViewer.css";
+const { TextArea } = Input;
 
 class RequestViewer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       requestdata: {},
-      requestFailed: false
+      requestFailed: false,
+      commentBox: ""
     };
   }
-  async componentDidMount() {
+  componentDidMount() {
+    this.loadRequestData();
+  }
+  loadRequestData = async () => {
     const { account } = this.props.user.user;
     const requestdata = await apiBackEnd(
       `requests/${account}/${this.props.id}`,
@@ -22,7 +28,28 @@ class RequestViewer extends Component {
       return this.setState({ requestFailed: true });
     }
     this.setState({ requestdata });
-  }
+  };
+  onCommentChange = event => {
+    this.setState({ commentBox: event.target.value });
+  };
+  onCommentSubmit = async () => {
+    if (!this.state.commentBox) {
+      return;
+    }
+    const { firstname, lastname, account, team } = this.props.user.user;
+    const response = await apiBackEnd("addrequestcomments", "post", {
+      id: this.props.id,
+      comments: this.state.commentBox,
+      account: account,
+      user: `${firstname} ${lastname}`,
+      team: team
+    });
+    if (response === "error") {
+      return console.log("Error Submitting Comment");
+    }
+    console.log("SUCCESS", response);
+    return this.loadRequestData();
+  };
   render() {
     if (this.state.requestFailed === true) {
       return <h1>Oops! This request doesn't exist</h1>;
@@ -38,7 +65,30 @@ class RequestViewer extends Component {
           <RequestForm {...this.props} {...this.state.requestdata} />
           <div className="requestviewer-box">
             Comments/Actions:
+            <Row>
+              <Col span={3} className="requestviewer-comments-label">
+                Time/Date:
+              </Col>
+              <Col span={5} className="requestviewer-comments-label">
+                User:
+              </Col>
+              <Col span={16} className="requestviewer-comments-label">
+                Comments:
+              </Col>
+            </Row>
             <RequestCommentList {...this.props} {...this.state.requestdata} />
+            <br />
+            <Row>
+              <Col span={2} style={{ textAlign: "center" }}>
+                <label>Comments:</label>
+              </Col>
+              <Col span={20}>
+                <TextArea placeholder="" onChange={this.onCommentChange} />
+              </Col>
+              <Col span={2} style={{ textAlign: "center" }}>
+                <Button onClick={this.onCommentSubmit}>Submit</Button>
+              </Col>
+            </Row>
           </div>
         </div>
       </div>
