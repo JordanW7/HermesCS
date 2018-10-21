21dd753c1e48c32012dc2bad2ebb950419c96df3
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-10-21 17:26:39 +0100

    request search box added for if you already know the ID number

--- src/components/Requests/SearchRequests/SearchRequests.js
+++ src/components/Requests/SearchRequests/SearchRequests.js
@@ -1,46 +1,94 @@
-import React from "react";
+import React, { Component } from "react";
 import RequestForm from "../../RequestForm/RequestForm";
 import "./SearchRequests.css";
-import { Row, Col } from "antd";
+import { Row, Col, Button, Form, Input } from "antd";
+import { Redirect } from "react-router-dom";
+const FormItem = Form.Item;
 
-const SearchRequests = props => {
-  return (
-    <div className="searchrequests">
-      <div className="searchrequests-contents">
-        <div className="searchrequests-title">Let's Search</div>
-        <RequestForm {...props} />
-        <div className="searchrequests-box">
-          Results
-          <Row>
-            <Col span={3} className="searchrequests-results-label">
-              Request#:
-            </Col>
-            <Col span={3} className="searchrequests-results-label">
-              Date:
-            </Col>
-            <Col span={3} className="searchrequests-results-label">
-              Created by:
-            </Col>
-            <Col span={3} className="searchrequests-results-label">
-              Topic
-            </Col>
-            <Col span={3} className="searchrequests-results-label">
-              For Team:
-            </Col>
-            <Col span={3} className="searchrequests-results-label">
-              Assigned to:
-            </Col>
-            <Col span={3} className="searchrequests-results-label">
-              Status:
-            </Col>
-            <Col span={3} className="searchrequests-results-label">
-              Last Update:
-            </Col>
-          </Row>
+class SearchRequests extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchRequestID: "",
+      searchRequestSubmit: false
+    };
+  }
+  onSearchRequestIDChange = event => {
+    this.setState({ searchRequestID: event.target.value });
+  };
+  onSearchRequestIDSubmit = event => {
+    this.setState({ searchRequestSubmit: true });
+  };
+  render() {
+    if (this.state.searchRequestSubmit) {
+      return (
+        <Redirect
+          to={{
+            pathname: `/requests/${this.state.searchRequestID}`,
+            state: "redirected"
+          }}
+        />
+      );
+    }
+    return (
+      <div className="searchrequests">
+        <div className="searchrequests-contents">
+          <div className="searchrequests-title">Let's Search</div>
+          <div className="searchrequests-requestidbox">
+            <Form layout="inline">
+              <Row>
+                <Col span={24}>
+                  <FormItem label="Know the request reference number? Enter it here:">
+                    <Input onChange={this.onSearchRequestIDChange} />
+                  </FormItem>
+                  <FormItem>
+                    <Button
+                      type="primary"
+                      onClick={this.onSearchRequestIDSubmit}
+                    >
+                      Go
+                    </Button>
+                  </FormItem>
+                </Col>
+              </Row>
+            </Form>
+          </div>
+          <RequestForm {...this.props} />
+          <div className="searchrequests-box">
+            <Row>
+              <Col span={24} className="searchrequests-results-label">
+                Results
+              </Col>
+              <Col span={3} className="searchrequests-results-label">
+                Request#:
+              </Col>
+              <Col span={3} className="searchrequests-results-label">
+                Date:
+              </Col>
+              <Col span={3} className="searchrequests-results-label">
+                Created by:
+              </Col>
+              <Col span={3} className="searchrequests-results-label">
+                Topic
+              </Col>
+              <Col span={3} className="searchrequests-results-label">
+                For Team:
+              </Col>
+              <Col span={3} className="searchrequests-results-label">
+                Assigned to:
+              </Col>
+              <Col span={3} className="searchrequests-results-label">
+                Status:
+              </Col>
+              <Col span={3} className="searchrequests-results-label">
+                Last Update:
+              </Col>
+            </Row>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SearchRequests;
