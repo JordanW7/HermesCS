499d8c9ce33ce70f8f956247fc02e45bc12ae8b2
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-10-24 21:19:31 +0100

    register api linkup complete

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,101 @@
 import React, { Component } from "react";
 import "./Register.css";
 import NavHeader from "../NavHeader/NavHeader";
-import { Icon, Input, Button, Tooltip } from "antd";
+import { Redirect } from "react-router-dom";
+import { Icon, Input, Button, Tooltip, message } from "antd";
+import apiBackEnd from "../../api/api";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      registerAccount: "",
+      registerFirstName: "",
+      registerLastName: "",
+      registerEmail: "",
+      registerPassword: "",
+      registerSuccess: false
+    };
+  }
+  onRegisterAccountChange = event => {
+    this.setState({ registerAccount: event.target.value });
+  };
+  onRegisterFirstNameChange = event => {
+    this.setState({ registerFirstName: event.target.value });
+  };
+  onRegisterLastNameChange = event => {
+    this.setState({ registerLastName: event.target.value });
+  };
+  onRegisterEmailChange = event => {
+    this.setState({ registerEmail: event.target.value });
+  };
+  onRegisterPasswordChange = event => {
+    this.setState({ registerPassword: event.target.value });
+  };
+  onRegisterSubmit = async () => {
+    const {
+      registerAccount,
+      registerPassword,
+      registerEmail,
+      registerFirstName,
+      registerLastName
+    } = this.state;
+    if (
+      !registerAccount ||
+      !registerEmail ||
+      !registerPassword ||
+      !registerFirstName ||
+      !registerLastName
+    ) {
+      return message.error("Please fill in all of the fields to register.");
+    }
+    const request = await apiBackEnd("register", "post", {
+      account: registerAccount,
+      email: registerEmail,
+      password: registerPassword,
+      firstname: registerFirstName,
+      lastname: registerLastName
+    });
+    if (request === "account exists") {
+      return message.error("Oops, this account already exists.");
+    }
+    if (request === "success") {
+      message.success("Account Created. Let's get you inside...");
+      const data = await apiBackEnd("signin", "post", {
+        account: registerAccount,
+        email: registerEmail,
+        password: registerPassword
+      });
+      if (data === "error" || !data.id) {
+        return;
+      }
+      window.localStorage.setItem("token", data.token);
+      const userdata = await apiBackEnd(
+        `profile/${data.account}/${data.id}`,
+        "get"
+      );
+      if (userdata === "error" || !userdata.id) {
+        return;
+      }
+      this.props.onLoadUser(userdata);
+      this.props.onSignin();
+      this.setState({ registerSuccess: true });
+      return message.success("Welcome to HermesCS!");
+    } else {
+      console.log(request);
+      return message.error("Oops, something went wrong. Please try again");
+    }
+  };
   render() {
+    if (this.state.registerSuccess) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/dashboard"
+          }}
+        />
+      );
+    }
     return (
       <div className="register-full">
         <NavHeader {...this.props} />
@@ -29,28 +120,37 @@ class Register extends Component {
                 <Icon type="login" style={{ color: "rgba(0,0,0,.25)" }} />
               }
               type="text"
+              onChange={this.onRegisterAccountChange}
             />
             <label>First Name:</label>
             <Input
               prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
               type="text"
+              onChange={this.onRegisterFirstNameChange}
             />
             <label>Last Name:</label>
             <Input
               prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
               type="text"
+              onChange={this.onRegisterLastNameChange}
             />
             <label>Email:</label>
             <Input
               prefix={<Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />}
               type="text"
+              onChange={this.onRegisterEmailChange}
             />
             <label>Password:</label>
             <Input
               prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
               type="password"
+              onChange={this.onRegisterPasswordChange}
             />
-            <Button type="primary" className="register-submit">
+            <Button
+              type="primary"
+              className="register-submit"
+              onClick={this.onRegisterSubmit}
+            >
               Register & Get Started!
             </Button>
           </div>
