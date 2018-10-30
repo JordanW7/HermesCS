6843db7492c08a0dcc737454815fa10f96d79c40
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-10-30 14:32:35 +0000

    settings - modifyuser UI complete basic

---
+++ src/components/Settings/Users/UserSettingsModify.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { Col, Form, Button, Select, message } from "antd";
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+class UserSettingsModify extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userSettingsModName: "",
+      userSettingsModAccess: "",
+      userSettingsModTeam: "",
+      userSettingsUserList: "",
+      userSettingsTeamList: ""
+    };
+  }
+  componentDidMount() {
+    this.loadUserSettingsModData();
+  }
+  loadUserSettingsModData = () => {
+    //Load list of teams for selection
+    //load list of users for selection
+    return;
+  };
+  onModUserNameChange = event => {
+    this.setState({ userSettingsModName: event.target.value });
+  };
+  onModUserTeamChange = event => {
+    this.setState({ userSettingsAddTeam: event.target.value });
+  };
+  onModUserSubmit = () => {
+    console.log("MAKE API CALL HERE");
+    message.success("API SENT");
+  };
+  render() {
+    return (
+      <Col span={18} className="settings-content-actions">
+        <div className="settings-content-title">Modify User</div>
+        <Form layout="inline">
+          <FormItem label="User:">
+            <Select
+              defaultValue="Please select"
+              onChange={this.onModUserNameChange}
+            >
+              {this.state.userSettingsUserList &&
+                this.state.userSettingsUserList.map((user, i) => {
+                  return (
+                    <Option key={`${i}_user`} value={`${user}`}>
+                      {user}
+                    </Option>
+                  );
+                })}
+            </Select>
+          </FormItem>
+
+          <FormItem label="New Team:">
+            <Select
+              defaultValue="Please select"
+              onChange={this.onModUserTeamChange}
+            >
+              {this.state.userSettingsTeamList &&
+                this.state.userSettingsTeamList.map((team, i) => {
+                  return (
+                    <Option key={`${i}_team`} value={`${team}`}>
+                      {team}
+                    </Option>
+                  );
+                })}
+            </Select>
+          </FormItem>
+          <Button type="primary" onClick={this.onAddUserSubmit}>
+            Update
+          </Button>
+          <Button type="primary" onClick={this.onAddUserSubmit}>
+            Disable/Renable Account
+          </Button>
+        </Form>
+      </Col>
+    );
+  }
+}
+
+export default UserSettingsModify;
