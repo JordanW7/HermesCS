be38f00f1fa8ca4e977a75109fe7fd6300b7450b
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-11-07 20:55:23 +0000

    web app url update

--- src/App.js
+++ src/App.js
@@ -138,6 +138,11 @@ class App extends Component {
     return (
       <Switch>
         <Route exact path="/" render={() => <Hero {...this.props} />} />
+        <Route
+          exact
+          path="/index.html"
+          render={() => <Hero {...this.props} />}
+        />
         <Route exact path="/about" render={() => <About {...this.props} />} />
         <Route
           exact
